fa2f897a93ceee8afbeccabf4d8edb01e183d62f
AlbertoParravicini <albyratta@gmail.com>
2019-10-01 12:13:11 +0200

    fixed page resizing

--- js/test.js
+++ js/test.js
@@ -7,11 +7,13 @@ let xvalues; // Using an array to store width values for the wave
 
 let line_height; // Effective length of the line;
 let heigth_offset = 120; // How much shorter the line is w.r.t. the canvas;
+let extra_height = 100; // How much the canvas height is extended;
 
 let radius_1 = 20;
 let radius_2 = 30;
 
-let circle_angle_offset = 0; // Used to animate the rotation of the circle;
+let circle_angle_offset_1 = 0; // Used to animate the rotation of the circle;
+let circle_angle_offset_2 = 0; // Used to animate the rotation of the circle;
 
 let emit_particles_frames = 50;
 let curr_emit_particles = 0;
@@ -28,123 +30,135 @@ let circlesmallx;
 let circlesmally;
 
 function setup() {
-  frameRate(30);
-	var canvasDiv = document.getElementById('drawing-box');
-	var width = canvasDiv.offsetWidth;
-  var height = canvasDiv.offsetHeight;
+	frameRate(30);
+  var canvasDiv = document.getElementById('drawing-box');
+  var contentDivs = document.getElementsByClassName('content');
+  var height = extra_height;
+  for (let i = 0; i < contentDivs.length; i++) {
+    height += contentDivs[i].offsetHeight;
+  }
+  var width = canvasDiv.offsetWidth;
+  // var height = canvasDiv.offsetHeight;
 	line_height = height - heigth_offset;
 	var canvas = createCanvas(width, height);
 	canvas.parent('drawing-canvas');
 
-		background(255, 255, 255);
-			computeWave();
-      renderWave();
-      circlesmallx = width / 2 + xvalues[xvalues.length - 1];
-      circlesmally = heigth_offset / 2 + (xvalues.length - 1) * yspacing
-    	circle_small = computeCircle(
-        circlesmallx,
-        circlesmally,
-        radius_2
-        );
+	background(255, 255, 255);
+	computeWave();
 }
+
+/////////////////////////////
+/////////////////////////////
 
 function windowResized() {
 	var canvasDiv = document.getElementById('drawing-box');
 	var width = canvasDiv.offsetWidth;
-  var height = canvasDiv.offsetHeight;
-  
-  line_height = height - heigth_offset;
-		resizeCanvas(width, height);
+  // var height = canvasDiv.offsetHeight;
+  var contentDivs = document.getElementsByClassName('content');
+  var height = extra_height;
+  for (let i = 0; i < contentDivs.length; i++) {
+    height += contentDivs[i].offsetHeight;
+  }
 
-		background(255, 255, 255);
-			computeWave();
-      renderWave();
-      circlesmallx = width / 2 + xvalues[xvalues.length - 1];
-      circlesmally = heigth_offset / 2 + (xvalues.length - 1) * yspacing
-    	circle_small = computeCircle(
-        circlesmallx,
-        circlesmally,
-        radius_2
-        );
+	line_height = height - heigth_offset;
+	resizeCanvas(width, height);
+
+	background(255, 255, 255);
+	computeWave();
 }
+
+/////////////////////////////
+/////////////////////////////
 
 function draw() {
-  stroke(30, 30, 30);
+  strokeWeight(1.5);
+	stroke(63, 62, 62);
 	background(255, 255, 255);
-  renderWave();
+	renderWave();
 
-  circle_angle_offset += 0.02
+	circle_angle_offset_1 += 0.02
+	circle_angle_offset_2 -= 0.02
 
-  var y = floor(mouseY / yspacing);
-  y = min(y, xvalues.length - 1);
-  y = max(0, y);
-  var circle_big = computeCircle(
-    width / 2 + xvalues[y],
-    heigth_offset / 2 + y * yspacing,
-    radius_1
-    );
-  drawCircle(circle_big);
-	drawCircle(circle_small);
+	var y = floor(mouseY / yspacing);
+	y = min(y, xvalues.length - 1);
+	y = max(0, y);
+	drawCircle(
+		width / 2 + xvalues[y],
+		heigth_offset / 2 + y * yspacing,
+		radius_1,
+		circle_angle_offset_1
+	);
+	circlesmallx = width / 2 + xvalues[xvalues.length - 1];
+	circlesmally = heigth_offset / 2 + (xvalues.length - 1) * yspacing
+	circle_small = drawCircle(
+		circlesmallx,
+		circlesmally,
+		radius_2,
+		circle_angle_offset_2
+	);
 
-  // We emit particles if the circles match;
-  if (y < xvalues.length - 1) {
-    emit = true;
-  } 
+	// We emit particles if the circles match;
+	if (y < xvalues.length - 1) {
+		emit = true;
+	}
 
-  // Reset the emission;
-  if (emit && (curr_emit_particles == 0) && (y == xvalues.length - 1)) {
-    curr_emit_particles = emit_particles_frames;
-    emit = false;
-    // Create some random angles;
-    for (let p = 0; p < particles_num; p++) {
-      angle = TWO_PI * Math.random();
-      particles_angles[p] = [cos(angle), sin(angle)]
-      particles_pos[p] = 0;
-      particle_color[p] = [floor(Math.random() * 255), floor(Math.random() * 255), floor(Math.random() * 255), floor(Math.random() * 255)]
-      particle_starts[p] = floor(Math.random() * emit_particles_frames / 2);
-    }
-  } 
-  // Perform an emission step;
-  if (curr_emit_particles > 0) {
-    // Move the particles;
-    for (let p = 0; p < particles_num; p++) {
-      // Delay the start of some particles;
-      if (emit_particles_frames - curr_emit_particles < particle_starts[p]) {
-        continue;
-      }
+	// Reset the emission;
+	if (emit && (curr_emit_particles == 0) && (y == xvalues.length - 1)) {
+		curr_emit_particles = emit_particles_frames;
+		emit = false;
+		// Create some random angles;
+		for (let p = 0; p < particles_num; p++) {
+			angle = TWO_PI * Math.random();
+			particles_angles[p] = [cos(angle), sin(angle)]
+			particles_pos[p] = 0;
+			particle_color[p] = [floor(Math.random() * 255), floor(Math.random() * 255), floor(Math.random() * 255), floor(Math.random() * 255)]
+			particle_starts[p] = floor(Math.random() * emit_particles_frames / 2);
+		}
+	}
+	// Perform an emission step;
+	if (curr_emit_particles > 0) {
+		// Move the particles;
+		for (let p = 0; p < particles_num; p++) {
+			// Delay the start of some particles;
+			if (emit_particles_frames - curr_emit_particles < particle_starts[p]) {
+				continue;
+			}
 
-      strokeWeight(4);
-      stroke(particle_color[p][0],
-        particle_color[p][1],
-        particle_color[p][2],
-        particle_color[p][3] * (curr_emit_particles / emit_particles_frames));
-      c = particles_angles[p][0];
-      s = particles_angles[p][1];
-      p_x_new = c * (particles_pos[p] + particles_mov);
-      p_y_new = s * (particles_pos[p] + particles_mov);
-      p_x_start = c * (particles_pos[p] - 3 * particles_mov);
-      p_y_start = s * (particles_pos[p] - 3 * particles_mov);
-      line(
-        p_x_start + circlesmallx,
-        p_y_start + circlesmally,
-        p_x_new + circlesmallx,
-        p_y_new + circlesmally
-      )
-      particles_pos[p] += particles_mov;
-    }
-    curr_emit_particles--;
-    strokeWeight(1);
-    stroke(30, 30, 30);
-  }
+			strokeWeight(4);
+			stroke(particle_color[p][0],
+				particle_color[p][1],
+				particle_color[p][2],
+				particle_color[p][3] * (curr_emit_particles / emit_particles_frames));
+			c = particles_angles[p][0];
+			s = particles_angles[p][1];
+			p_x_new = c * (particles_pos[p] + particles_mov);
+			p_y_new = s * (particles_pos[p] + particles_mov);
+			p_x_start = c * (particles_pos[p] - 3 * particles_mov);
+			p_y_start = s * (particles_pos[p] - 3 * particles_mov);
+			line(
+				p_x_start + circlesmallx,
+				p_y_start + circlesmally,
+				p_x_new + circlesmallx,
+				p_y_new + circlesmally
+			)
+			particles_pos[p] += particles_mov;
+		}
+		curr_emit_particles--;
+		strokeWeight(1.5);
+		stroke(63, 62, 62);
+	}
 }
+
+/////////////////////////////
+/////////////////////////////
 
 function computeWave() {
 	amplitude = width / 5;
 	radius_1 = max(width / 9, 12);
 	radius_2 = max(width / 14, 7);
 	dy = (TWO_PI / period) * yspacing;
-		xvalues = new Array(floor(line_height / yspacing));
-		calcWave();
+	xvalues = new Array(floor(line_height / yspacing));
+	calcWave();
 }
 
 function calcWave() {
@@ -158,7 +172,7 @@ function calcWave() {
 
 function renderWave() {
 	stroke(30, 30, 30);
-	for (let y = 0; y < xvalues.length - 1; y=y+2) {
+	for (let y = 0; y < xvalues.length - 1; y = y + 2) {
 		line(width / 2 + xvalues[y],
 			heigth_offset / 2 + y * yspacing,
 			width / 2 + xvalues[y + 1],
@@ -166,18 +180,13 @@ function renderWave() {
 	}
 }
 
-
-function computeCircle(x, y, radius) {
-  N = 50;
+function drawCircle(x, y, radius, offset) {
+	N = 50;
 	values = new Array(N);
 
 	for (let i = 0; i < values.length; i++) {
-		values[i] = [x + cos(i * TWO_PI / N + circle_angle_offset) * radius, y + sin(i * TWO_PI / N + circle_angle_offset) * radius]
-  }
-  return values;
-}
-
-function drawCircle(values) {
+		values[i] = [x + cos(i * TWO_PI / N + offset) * radius, y + sin(i * TWO_PI / N + offset) * radius]
+	}
 	for (let i = 0; i < values.length - 1; i = i + 2) {
 		line(values[i][0], values[i][1], values[i + 1][0], values[i + 1][1])
 	}

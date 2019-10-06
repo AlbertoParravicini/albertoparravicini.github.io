fe6cf7f1390ae0a644933676cecac9c694b7a77b
AlbertoParravicini <albyratta@gmail.com>
2019-10-06 12:01:47 +0200

    added image scroll

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,24 @@
 // Get the modal;
 var modal = document.getElementById("modal-img-container");
 
+// Current image in the modal;
+curr_image = 0;
+
 // Get the image and insert it inside the modal;
-var img = document.getElementsByClassName("image-zoomable");
+var images = document.getElementsByClassName("image-zoomable");
 var modalImg = document.getElementById("modal-img");
-for (let i = 0; i < img.length; i++) {
-  img[i].onclick = function(){
+for (let i = 0; i < images.length; i++) {
+  images[i].onclick = function(){
     modal.style.display = "flex";
     modal.style.justifyContent = "center";
     modal.style.alignItems = "center";
     modalImg.src = this.src;
+    curr_image = i;
   }
 }
 
 // Get the <span> element that closes the modal;
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close-modal")[0];
 
 // When the user clicks on <span> (x), close the modal;
 span.onclick = function() {
@@ -31,5 +35,20 @@ $('#modal-img-container').click(function() {
 });
 
 $('#modal-img').click(function(e) {
+  e.stopPropagation();
+});
+
+// Move across images by clicking on the ">" & "<";
+$('#right-arrow').click(function(e) {
+  curr_image = (curr_image + 1) % images.length;
+  modalImg.src = images[curr_image].src;
+  e.stopPropagation();
+});
+$('#left-arrow').click(function(e) {
+  curr_image--;
+  if (curr_image < 0) {
+    curr_image = images.length - 1;
+  }
+  modalImg.src = images[curr_image].src;
   e.stopPropagation();
 });

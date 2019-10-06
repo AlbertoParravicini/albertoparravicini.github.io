d4e7b7251db1ce7851a475862ae883eb938f712d
AlbertoParravicini <albyratta@gmail.com>
2019-10-06 11:26:52 +0200

    added close picture by clocking on background

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,7 @@
-// Get the modal
+// Get the modal;
 var modal = document.getElementById("modal-img-container");
 
-// Get the image and insert it inside the modal
+// Get the image and insert it inside the modal;
 var img = document.getElementsByClassName("image-zoomable");
 var modalImg = document.getElementById("modal-img");
 for (let i = 0; i < img.length; i++) {
@@ -13,12 +13,23 @@ for (let i = 0; i < img.length; i++) {
   }
 }
 
-// Get the <span> element that closes the modal
+// Get the <span> element that closes the modal;
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x), close the modal;
 span.onclick = function() {
   modal.style.display = "none";
   modal.style.justifyContent = "";
   modal.style.alignItems = "";
 }
+
+// Close zoomed images by clicking on background;
+$('#modal-img-container').click(function() {
+  modal.style.display = "none";
+  modal.style.justifyContent = "";
+  modal.style.alignItems = "";
+});
+
+$('#modal-img').click(function(e) {
+  e.stopPropagation();
+});
